1e6cca884972988c2dfbcc116d37f2893de1b1a0
Tyler <Tyl3r.s@outlook.com>
2023-10-15 11:59:39 -0400

    Refactor react component

---
+++ projekt/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Planner from './components/Planner';
+import './App.css';
+
+function App() {
+  return (
+    <Planner />
+  );
+}
+
+export default App;
