b8c090081c8e26b49bf4b19a0f4a0e45a32bf8c7
Tyler <Tyl3r.s@outlook.com>
2023-10-15 12:34:09 -0400

    add array to populate timeblock props

--- projekt/src/pages/Planner.jsx
+++ projekt/src/pages/Planner.jsx
@@ -2,11 +2,75 @@ import React from 'react';
 import Banner from './components/Banner';
 import Timeblock from './components/Timeblock';
 
+const timeBlockArr = [
+    {
+        "time": "7am",
+        "tasks": "task"
+    },
+    {
+        "time": "8am",
+        "tasks": "task"
+    },
+    {
+        "time": "9am",
+        "tasks": "task"
+    },
+    {
+        "time": "10am",
+        "tasks": "task"
+    },
+    {
+        "time": "11am",
+        "tasks": "task"
+    },
+    {
+        "time": "12pm",
+        "tasks": "task"
+    },
+    {
+        "time": "1pm",
+        "tasks": "task"
+    },
+    {
+        "time": "2pm",
+        "tasks": "task"
+    },
+    {
+        "time": "3pm",
+        "tasks": "task"
+    },
+    {
+        "time": "4pm",
+        "tasks": "task"
+    },
+    {
+        "time": "5pm",
+        "tasks": "task"
+    },
+    {
+        "time": "6pm",
+        "tasks": "task"
+    },
+    {
+        "time": "7pm",
+        "tasks": "task"
+    }
+]
+
 export default function Planner() {
     return (
         <div className="App">
             <Banner />
-            <Timeblock />
+            <div className="timeBlocks">
+                {timeBlockArr.map(x => {
+                    return (
+                        <Timeblock 
+                            time={x.time}
+                            tasks={x.tasks}
+                        />
+                    )
+                })}
+            </div>
         </div>
     )
 }
--- projekt/src/pages/components/Timeblock.jsx
+++ projekt/src/pages/components/Timeblock.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-export default function Timeblock() {
+export default function Timeblock(props) {
     return (
         <div className="PlannerContainer">
             <form className="timeBlock d-flex justify-content-center">
-                <div className="">
-                    <label className="">7am</label>
-                    <textarea className=""></textarea>
+                <div className="Banner">
+                    <label className="">{props.time}</label>
+                    <textarea className="">{props.tasks}</textarea>
                     <button type="button" className="saveBtn">Save</button>
                 </div>
             </form>
